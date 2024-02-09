5dc36c9a2fe19e6ae11e777e306a38f80b1063f8
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-02-09 22:00:09 +0530

    getting started with react forms

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [formData, setFormData] = useState({ firstName: "" });
+  function changeHandler(event) {
+    const { name, value, checked, type } = event.target;
+    setFormData((prevdata) => ({
+      ...prevdata,
+      [name]: value,
+    }));
+  }
+  console.log(formData);
+  return (
+    <div className="flex flex-col items-center mt-2">
+      <form>
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          placeholder="Gaurav"
+          value={formData.firstName}
+          onChange={changeHandler}
+          className="outline"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default App;
