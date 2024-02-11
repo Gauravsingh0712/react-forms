3506bc1951ff0c979fbc7a436e816cd4d3e46de1
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-02-11 19:29:19 +0530

    completed react forms

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [formData, setFormData] = useState({ firstName: "" });
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    country: "",
+    streetAddress: "",
+    state: "",
+    city: "",
+    postalCode: "",
+    comments: false,
+    candidates: false,
+    offers: false,
+    pushNotification: "",
+  });
   function changeHandler(event) {
     const { name, value, checked, type } = event.target;
     setFormData((prevdata) => ({
       ...prevdata,
-      [name]: value,
+      [name]: type === "checked" ? checked : value,
     }));
   }
-  console.log(formData);
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log("finally printing the form data : ");
+    console.log(formData);
+  }
   return (
     <div className="flex flex-col items-center mt-2">
-      <form>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="firstName">First name</label>
         <br />
         <input
           type="text"
@@ -24,6 +42,184 @@ const App = () => {
           onChange={changeHandler}
           className="outline"
         />
+
+        <br />
+
+        <label htmlFor="lastName">Last name</label>
+        <br />
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          placeholder="Singh"
+          value={formData.lastName}
+          onChange={changeHandler}
+          className="outline"
+        />
+
+        <br />
+
+        <label htmlFor="email">Email Address</label>
+        <br />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="xyz@email.com"
+          value={formData.email}
+          onChange={changeHandler}
+          className="outline"
+        />
+        <br />
+        <label htmlFor="country">Country</label>
+        <br />
+        <select
+          name="country"
+          id="country"
+          onChange={changeHandler}
+          value={formData.country}
+        >
+          <option>India</option>
+          <option>United kingdom</option>
+          <option>Russia</option>
+          <option>United States</option>
+        </select>
+
+        <br />
+
+        <label htmlFor="streetAddress">Street Address</label>
+        <br />
+        <input
+          type="text"
+          name="streetAddress"
+          id="streetAddress"
+          placeholder="main road delhi"
+          value={formData.streetAddress}
+          onChange={changeHandler}
+          className="outline"
+        />
+
+        <br />
+
+        <label htmlFor="state">State</label>
+        <br />
+        <input
+          type="text"
+          name="state"
+          id="state"
+          placeholder="Bihar"
+          value={formData.state}
+          onChange={changeHandler}
+          className="outline"
+        />
+
+        <br />
+
+        <label htmlFor="city">City</label>
+        <br />
+        <input
+          type="text"
+          name="city"
+          id="city"
+          placeholder="Patna"
+          value={formData.city}
+          onChange={changeHandler}
+          className="outline"
+        />
+        <br />
+
+        <label htmlFor="postalCode">Postal Code</label>
+        <br />
+        <input
+          type="text"
+          name="postalCode"
+          id="postalCode"
+          placeholder="123456"
+          value={formData.postalCode}
+          onChange={changeHandler}
+          className="outline"
+        />
+
+        <fieldset>
+          <legend>By Email</legend>
+          <div className="flex">
+            <input
+              id="comments"
+              name="comments"
+              type="checkbox"
+              checked={formData.comments}
+              onChange={changeHandler}
+            />
+            <div>
+              <label htmlFor="comments">Comments</label>
+              <p>Get notified when someones posts a comment on a posting</p>
+            </div>
+          </div>
+
+          <div className="flex">
+            <input
+              id="candidates"
+              name="candidates"
+              type="checkbox"
+              checked={formData.candidates}
+              onChange={changeHandler}
+            />
+            <div>
+              <label htmlFor="candidates">Candidates</label>
+              <p>Get notified when a candidate applies for a job</p>
+            </div>
+          </div>
+
+          <div className="flex">
+            <input
+              id="offers"
+              name="offers"
+              type="checkbox"
+              checked={formData.offers}
+              onChange={changeHandler}
+            />
+            <div>
+              <label htmlFor="offers">Offers</label>
+              <p>Get notified when someones posts a comment on a posting</p>
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>Push Notification</legend>
+          <p>These are d............phone</p>
+
+          <input
+            type="radio"
+            id="pushEverything"
+            name="pushNotification"
+            value="Everything"
+            onChange={changeHandler}
+          />
+          <label htmlFor="pushEverything">Everything</label>
+          <br />
+          <input
+            type="radio"
+            id="pushEmail"
+            name="pushNotification"
+            value="Same as email"
+            onChange={changeHandler}
+          />
+          <label htmlFor="pushEmail">Same as Email</label>
+          <br />
+          <input
+            type="radio"
+            id="pushNothing"
+            name="pushNotification"
+            value="No Push Notification"
+            onChange={changeHandler}
+          />
+          <label htmlFor="pushNothing">No Push Notification</label>
+        </fieldset>
+
+        <button className="bg-blue-500 text-white font-bold rounded py-2 px-4">
+          Save
+        </button>
       </form>
     </div>
   );
